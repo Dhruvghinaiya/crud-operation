e15f820be18eeb06abd4dd332469799626ffa431
Dhruv Ghinaiya <dhruvghinaiya793@gmail.com>
2026-02-09 12:13:08 +0530

    make a migration and model files

---
+++ database/migrations/2026_02_09_062352_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('gender');
+            $table->string('mobile');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->string('avatar')->nullable();
+            $table->date('dob');
+            $table->boolean('is_active');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
